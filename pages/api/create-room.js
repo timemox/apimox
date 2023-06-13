96ceb0913912ef4c277823748fd9b7728222da25
glucoso <oriol@ateneatech.com>
2023-06-13 16:04:33 +0200

    up

--- pages/api/create-room.js
+++ pages/api/create-room.js
@@ -1,19 +1,32 @@
+// pages/api/rooms/create-room.js
 
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const uri = 'mongodb+srv://glucoso:Xdcjt33Fvc7jwShP@mox.l1obwjz.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { title, description, coordinates } = req.body;
 
-    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
     try {
+      // Connect the client to the server
       await client.connect();
 
-      const collection = client.db("<dbname>").collection("rooms");
+      // Choose your database
+      const db = client.db("<dbname>");
 
+      // Choose your collection within the database
+      const collection = db.collection("rooms");
+
+      // Insert a single document into your collection
       const result = await collection.insertOne({
         title,
         description,
@@ -24,9 +37,12 @@ export default async function handler(req, res) {
     } catch (error) {
       res.status(500).json({ error: 'Error connecting to MongoDB', details: error });
     } finally {
+      // Ensures that the client will close when you finish/error
       await client.close();
     }
   } else {
     res.status(405).send('Method not allowed');
   }
 }
+
+
