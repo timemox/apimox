97f8ea6310952f1e2c4bf5e6e9e73e3e1cd99499
glucoso <oriol@ateneatech.com>
2023-06-13 15:55:27 +0200

    up

--- pages/api/create-room.js
+++ pages/api/create-room.js
@@ -1,17 +1,31 @@
-// pages/api/rooms/create-room.js
 
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+const uri = 'mongodb+srv://glucoso:Xdcjt33Fvc7jwShP@mox.l1obwjz.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { title, description, coordinates } = req.body;
 
-    // Aquí puedes agregar la lógica para guardar la nueva sala en tu base de datos
-    // Por ahora, solo devolveremos la nueva sala como una respuesta
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-    res.status(200).json({
-      title,
-      description,
-      coordinates,
-    });
+    try {
+      await client.connect();
+
+      const collection = client.db("<dbname>").collection("rooms");
+
+      const result = await collection.insertOne({
+        title,
+        description,
+        coordinates,
+      });
+
+      res.status(200).json(result.ops[0]);
+    } catch (error) {
+      res.status(500).json({ error: 'Error connecting to MongoDB', details: error });
+    } finally {
+      await client.close();
+    }
   } else {
     res.status(405).send('Method not allowed');
   }
